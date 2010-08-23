ffc588cddd3e222989f13046f00ce99badef7651
David Laing <david@davidlaing.com>
2010-08-23 23:35:36 +0100

    Better koan test runner, and scripts to run from command line

---
+++ src-test/PathToEnlightenment.js
@@ -0,0 +1,40 @@
+var jstestdriver; //global variables
+
+var koans = new {
+		all: [],
+		add: function (groupName, functionName, functionPtr) {
+				this.all.push({group: groupName, name: functionName, fn: functionPtr});
+			}
+};
+   
+var koanTestCases = new TestCase("JavascriptKoans", {
+	testYourKarma: function () { 
+		var advanceToNextKoan = true;
+		var previousGroup = "";
+		var i = 0; 
+		while (advanceToNextKoan && i < koans.all.length) {
+			if (koans.all[i].group !== previousGroup) {
+				jstestdriver.console.log("");
+				jstestdriver.console.log("Thinking about " + koans.all[i].group);
+				previousGroup = koans.all[i].group;
+			}
+			
+			try {
+				koans.all[i].fn(); 
+				jstestdriver.console.log("    " + koans.all[i].name + " has increased your awareness"); 
+				i += 1; 
+			} catch (e) {
+				advanceToNextKoan = false;
+				
+				var msg = koans.all[i].name + " has damaged your karma\n\n" 
+						+ "You have not yet reached enlightenment:\n"
+						+ e.message
+						+ "\n\n"
+				        + "Meditate on the following code:\n"
+				        + koans.all[i].group + " :: " + koans.all[i].name;
+				jstestdriver.console.log(msg);
+				fail(msg);
+			} 
+		}
+	}
+}); 
